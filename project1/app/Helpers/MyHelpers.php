727dfa28a29eee14a599a1b3aeb70a40d10309cb
NFORSHIFU234 Dev || Shifu-Nfor Nyuiring-yoh Rhagninyui <75176952+nforshifu234dev@users.noreply.github.com>
2024-01-18 19:26:14 +0100

    Added a helper that will help to generate random strings for userIds and the rest

---
+++ project1/app/Helpers/MyHelpers.php
@@ -0,0 +1,15 @@
+// File: app/Helpers/MyHelpers.php
+<?php
+
+if (!function_exists('generateRandomString')) {
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        return $randomString;
+    }
+}
